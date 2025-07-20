811121352c44326ccd4e70266f23ed041b2a2dca
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-07-20 16:53:19 +0200

    Remove dist directory from Git tracking

--- dist/types/index.js
+++
@@ -1,5 +0,0 @@
-export { AppsScriptEventType } from "./AppsScriptEventType";
-export { HeaderAcceptMimeType } from "./HeaderAcceptMimeType";
-export { HttpStatus } from "./HttpStatus";
-export { ParamSource } from "./ParamSource";
-export { RequestMethod } from "./RequestMethod";
