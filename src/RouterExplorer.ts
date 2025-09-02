79339ae8109af400ed2e1eb3580f2d2f2cb1946a
Maksym Stoianov <stoianov.maksym@gmail.com>
2025-09-02 23:26:19 +0200

    fix: controllers

--- src/RouterExplorer.ts
+++ src/RouterExplorer.ts
@@ -3,28 +3,18 @@ import { normalize } from "appsscript-utils";
 import {
   CONTROLLER_OPTIONS_METADATA,
   CONTROLLER_TYPE_METADATA,
-  CONTROLLER_WATERMARK,
   METHOD_METADATA,
   PATH_METADATA
 } from "./config/constants";
 import { Newable, RouteMetadata } from "./types";
 
 export class RouterExplorer {
-  static explore(controllers: Newable[] = []): RouteMetadata[] {
+  static explore(controllers: Map<Newable, unknown>): RouteMetadata[] {
     const routes: RouteMetadata[] = [];
 
-    for (const controller of controllers) {
-      const isController = Reflect.getMetadata(
-        CONTROLLER_WATERMARK,
-        controller
-      );
-
-      if (!isController) continue;
-
-      const controllerType: string | undefined = Reflect.getMetadata(
-        CONTROLLER_TYPE_METADATA,
-        controller
-      );
+    for (const controller of controllers.keys()) {
+      const controllerType: string | null =
+        Reflect.getMetadata(CONTROLLER_TYPE_METADATA, controller) || null;
 
       const isHttpController = controllerType === "http";
 
